2e9197458d216ca30942b03c215166325c5cc891
Yogeshwar Suryavamshi <yogi.s75@gmail.com>
2022-09-05 21:41:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+
+import "./App.css";
+
+function App() {
+  const [lat, setLat] = useState([]);
+  const [lon, setLon] = useState([]);
+
+  return <div className="App"></div>;
+}
+
+export default App;
